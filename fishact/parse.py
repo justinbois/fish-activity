3fd2d0f35eadcd1f6c378a306062d0724e10bf67
Justin Bois <bois@caltech.edu>
2017-03-30 14:32:35 -0700

    Updated resampling.

--- fishact/parse.py
+++ fishact/parse.py
@@ -1,7 +1,13 @@
 import datetime
 
+try:
+    import tqdm
+except:
+    pass
+
 import numpy as np
 import pandas as pd
+import numba
 
 
 def tidy_data(activity_name, genotype_name, out_name, lights_on, lights_off,
@@ -271,11 +277,57 @@ def load_activity(fname, genotype_fname, lights_on, lights_off, day_in_the_life,
     return df
 
 
-def load_perl_processed_activity(activity_file, df_gt):
+def load_perl_processed_activity(fname, genotype_fname, lights_off=14.0):
     """
-    Load activity data into tidy DataFrame
+    Load activity data into tidy DataFrame from Prober lab Perl script.
+
+    Parameters
+    ----------
+    fname : string
+        CSV file containing the activity data. This is a conversion
+        to CSV of the Excel file that comes off the instrument.
+    genotype_fname : string
+        File containing genotype information. This is in standard
+        Prober lab format, with tab delimited file.
+        - First row discarded
+        - Second row contains genotypes. String is only kept up to
+          the last space because they typically appear something like
+          'tph2-/- (n=20)', and we do not need the ' (n=20)'.
+        - Subsequent rows containg wells in the 96 well plate
+          corresponding to each genotype.
+    lights_off : float, default 14.0
+        The time where lights come on each day according to the
+        Zeitgeber time, in units of hours.
+
+    Returns
+    -------
+    df : pandas DataFrame
+        Tidy DataFrame with columns:
+        - activity: The activity as given by the instrument, based
+          on the `middur` columns of the inputted data set
+        - time: time in proper datetime format, based on the `sttime`
+          column of the inputted data file
+        - fish: ID of the fish
+        - genotype: genotype of the fish
+        - zeit: Zeitgeber time, based on the `start` column of the
+          inputted data file
+        - zeit_ind: an index for the Zeitgeber time. Because of some
+          errors in the acquisition, sometimes the times do not
+          perfectly line up. zeit_ind is just the index of the
+          measurement. This is needed for computing averages over
+          fish at each time point.
+        - light: True if the light is on.
+        - day: The day in the life of the fish
     """
-    df = pd.read_csv(activity_file, delimiter='\t', comment='#', header=[0, 1])
+
+    if lights_off < 0 or lights_off >= 24:
+        raise RuntimeError('Invalid lights_off.')
+
+    # Load the genotypes file
+    df_gt = load_gtype(genotype_fname)
+
+    # Load in the data set
+    df = pd.read_csv(fname, delimiter='\t', comment='#', header=[0, 1])
 
     # Make list of columns (use type conversion to allow list concatenation)
     df.columns = list(df.columns.get_level_values(1)[:2]) \
@@ -298,7 +350,7 @@ def load_perl_processed_activity(activity_file, df_gt):
     df = df.drop(cols_to_drop, axis=1)
 
     # Add a column for whether or not it is light
-    df['light'] = pd.Series(df.CLOCK < 14.0, index=df.index)
+    df['light'] = pd.Series(df.CLOCK < lights_off, index=df.index)
 
     # Find where the lights switch from off to on.
     dark_to_light = np.where(np.diff(df['light'].astype(np.int)) == 1)[0]
@@ -314,7 +366,7 @@ def load_perl_processed_activity(activity_file, df_gt):
     # Insert the day numnber into DataFrame
     df['day'] = pd.Series(day, index=df.index)
 
-    # Build ziet and put it in the DataFrame
+    # Build zeit and put it in the DataFrame
     zeit = 24.0 * df['day'] + df['CLOCK']
     df['zeit'] = pd.Series(zeit, index=df.index)
 
@@ -354,7 +406,54 @@ def load_perl_processed_activity(activity_file, df_gt):
     return df
 
 
-def resample(df, ind_win):
+@numba.jit(nopython=True)
+def _resample_array(x, ind_win):
+    """
+    Resample a NumPy array.
+    """
+    if len(x) == 0:
+        raise RuntimeError('`x` must be nonempty.')
+
+    # If the resampling window exceeds length of the array
+    if len(x) < ind_win:
+        return np.array([np.mean(x) * ind_win])
+
+    # Perform the resampling
+    if len(x) % ind_win == 0:
+        re_x = np.empty(len(x) // ind_win)
+        for i in range(len(x) // ind_win):
+            re_x[i] = np.sum(x[i*ind_win:(i+1)*ind_win])
+    else:
+        re_x = np.empty(len(x) // ind_win + 1)
+        for i in range(len(x) // ind_win):
+            re_x[i] = np.sum(x[i*ind_win:(i+1)*ind_win])
+        re_x[-1] = np.mean(x[ind_win*(len(x)//ind_win):]) * ind_win
+
+    return re_x
+
+
+def _resample_segment(df, ind_win, signal):
+    """
+    Resample a single sorted sequential time course of a DataFrame.
+    """
+    # Convert signal to list
+    if type(signal) not in [list, tuple]:
+        signal = [signal]
+
+    # Make DataFrame to hold resampled data
+    inds = df.index[::ind_win]
+    cols = [col for col in df.columns if col not in signal]
+    re_df = df.loc[inds, cols].reset_index(drop=True)
+
+    # Resample signal
+    for col in signal:
+        re_df[col] = _resample_array(df[col].values, ind_win)
+
+    return re_df
+
+
+
+def resample(df, ind_win, signal=['activity'], quiet=False):
     """
     Resample the DataFrame.
 
@@ -363,6 +462,13 @@ def resample(df, ind_win):
     df : pandas DataFrame
         DataFrame with pertinent data. Must have columns 'time',
         'fish', 'genotype', 'day', 'light', 'zeit'.
+    ind_win : int
+        Window for resampling, in units of indices.
+    signal : list
+        List of columns in the DataFrame to resample. These are
+        the signals, e.g., ['activity', 'midct'], to resample.
+    quiet : bool, default False
+        If True, status output to the screen is silenced.
 
     Returns
     -------
@@ -387,56 +493,40 @@ def resample(df, ind_win):
     if ind_win == 1:
         return df_in
 
-    # Extract  light
-    light = df_in.loc[df_in['fish']==df_in['fish'].unique()[0], 'light'].values
+    # Set up output DataFrame
+    df_out = pd.DataFrame(columns=df_in.columns)
 
-    # Find first light switching event
-    if light[0]:
-        where_false = np.where(~light)[0]
-        if len(where_false) == 0:
-            first_ind = 0
-        else:
-            first_ind = where_false[0]
+    if not quiet:
+        try:
+            iterator = tqdm.tqdm(df_in['fish'].unique())
+        except:
+            iterator = df_in['fish'].unique()
+        print('Performing resampling....')
     else:
-        where_true = np.where(light)[0]
-        if len(where_true) == 0:
-            first_ind = 0
-        else:
-            first_ind = where_true[0]
+        iterator = df_in['fish'].unique()
 
-    # Determine start index for averaging
-    if first_ind < ind_win:
-        start_ind = first_ind
-    else:
-        start_ind = first_ind % ind_win
+    for fish in iterator:
+        # Slice out entry for fish
+        df_fish = df_in.loc[
+                    df_in['fish']==fish, :].copy().reset_index(drop=True)
 
-    # Make GroupBy object
-    df_gb = df_in.groupby('fish')['activity']
+        # Find indices where light switches
+        df_fish['switch'] = df_fish['light'].diff()
+        df_fish.loc[df_fish.index[0], 'switch'] = True
+        inds = df_fish.where(df_fish['switch']).dropna().index
 
-    # Compute rolling sum (result is stored at right end of window)
-    s = df_gb.rolling(window=ind_win).sum().reset_index(level=0, drop='fish')
+        # Resample data for each segment
+        for i, ind in enumerate(inds[:-1]):
+            new_df = resample_segment(
+                            df_fish.loc[ind:inds[i+1]-1, :], ind_win, signal)
+            new_df = new_df.drop('switch', 1)
+            df_out = df_out.append(new_df, ignore_index=True)
+        new_df = resample_segment(df_fish.loc[inds[-1]:, :], ind_win, signal)
+        new_df = new_df.drop('switch', 1)
+        df_out = df_out.append(new_df, ignore_index=True)
 
-    # Columns to keep in output DataFrame
-    new_cols = ['time', 'fish', 'genotype', 'day', 'light', 'zeit']
+    # Make sure the data types are ok
+    for col in df_in:
+        df_out[col] = df_out[col].astype(df_in[col].dtype)
 
-    # Inds to keep
-    inds = np.array([])
-    win_inds = np.array([])
-    for fish in df_in.fish.unique():
-        start = df_in.loc[df_in.fish==fish, :].index[0] \
-                                            + start_ind + ind_win - 1
-        stop = df_in.loc[df_in.fish==fish, :].index[-1] + 1
-        new_inds = np.arange(start, stop, ind_win)
-        inds = np.concatenate((inds, new_inds - ind_win + 1))
-        win_inds = np.concatenate((win_inds, new_inds))
-
-    # Zeit indices
-    n_fish = len(df_in.fish.unique())
-    zeit_ind = list(range(int(len(inds) // n_fish))) * n_fish
-
-    # New DataFrame
-    df_resampled = df_in.loc[inds, new_cols].reset_index(drop=True)
-    df_resampled['activity'] = s[win_inds].values
-    df_resampled['zeit_ind'] = zeit_ind
-
-    return df_resampled
+    return df_out
