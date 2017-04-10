6a90cbedb68f3b3d678a5aa18c136ab766be084b
Justin Bois <bois@caltech.edu>
2017-04-10 00:53:49 -0700

    Added sleep latency and sleep bouts to summary.

--- fishact/summarize.py
+++ fishact/summarize.py
@@ -2,11 +2,131 @@ import numpy as np
 import pandas as pd
 import numba
 
+def _compute_bouts(df, rest=True):
+    """
+    Compute bout lengths for either sleep or active bouts.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        Minimally with columns 'sleep', 'day', 'light', 'exp_time', 'time'.
+
+    Returns
+    -------
+    output : float
+        sleep latency in units of experimental time (hours)
+
+    Notes
+    -----
+    .. We do not consider bouts at the beginning or end of an
+       experiment.
+    """
+
+    # Output DataFrame
+    cols = {'day_start': int,
+            'day_end': int,
+            'light_start': bool,
+            'light_end': bool,
+            'bout_start_exp': float,
+            'bout_end_exp': float,
+            'bout_start_clock': '<M8[ns]',
+            'bout_end_clock': '<M8[ns]',
+            'bout_length': float}
+    df_out = pd.DataFrame(columns=[key for key in cols])
+
+    # Get Boolean NumPy array for sleep
+    sleep = df['sleep'].values.astype(bool)
+
+    # Compute where switching between sleep and awake
+    switches = np.where(np.diff(sleep))[0] + 1
+
+    # Make sure there is a bout to include
+    if len(switches) < 2:
+        return df_out
+
+    # Is fish starting out and ending asleep?
+    start_asleep = bool(df['sleep'].iloc[0])
+    end_asleep = bool(df['sleep'].iloc[-1])
+
+    # Determine which switches to iterate over
+    # Equiv. to range(int(not(start_asleep ^ rest)), len(switches-1), 2)
+    if start_asleep:
+        if rest:
+            iterator = range(1, len(switches)-1, 2)
+        else:
+            iterator = range(0, len(switches)-1, 2)
+    else:
+        if rest:
+            iterator = range(0, len(switches)-1, 2)
+        else:
+            iterator = range(1, len(switches)-1, 2)
+
+    # Build DataFrame
+    for i in iterator:
+        ind1 = df.index[switches[i]]
+        ind2 = df.index[switches[i+1]]
+        new_row = {'day_start': df.loc[ind1, 'day'],
+                   'day_end': df.loc[ind2, 'day'],
+                   'light_start': df.loc[ind1, 'light'],
+                   'light_end': df.loc[ind2, 'light'],
+                   'bout_start_exp': df.loc[ind1, 'exp_time'],
+                   'bout_end_exp': df.loc[ind2, 'exp_time'],
+                   'bout_start_clock': df.loc[ind1, 'time'],
+                   'bout_end_clock': df.loc[ind2, 'time'],
+            'bout_length': df.loc[ind2, 'exp_time'] - df.loc[ind1, 'exp_time']}
+        df_out = df_out.append(new_row, ignore_index=True)
+
+    # Ensure data types
+    for col, dtype in cols.items():
+        df_out[col] = df_out[col].astype(dtype)
+
+    return df_out
+
+
 def _sleep_latency(df):
-    sleep_mins = df[df['sleep']==1]
+    """
+    Compute sleep latency.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        Minimally with columns 'sleep' and 'exp_time'.
+
+    Returns
+    -------
+    output : float
+        sleep latency in units of experimental time (hours)
+
+    Notes
+    -----
+    .. Sleep latency is defined as the length of time between the first
+       not-sleeping to sleeping transition and the first awake minute
+       after a light switching event. If there is no not-sleeping to
+       sleeping transition before the next light switching event, the
+       latency is NaN.
+    .. The inputted DataFrame contains a single record that starts at
+       the light switching event and ends at the last time point before
+       the next light switching event.
+    """
+    # When was the fish awake?
+    awake_mins = df[df['sleep']==0]
+
+    # If always awake, return NaN
+    if len(awake_mins) == 0:
+        return np.nan
+
+    # First awake minute
+    first_awake_min = awake_mins['exp_time'].min()
+
+    # Relevant sleep minutes
+    sleep_mins = df.loc[(df['sleep']==1) & (df['exp_time'] > first_awake_min)]
+
+    # If never asleep after first awake minute, return NaN
     if len(sleep_mins) == 0:
         return np.nan
-    return df.loc[sleep_mins.index[0], 'exp_time'] - df['exp_time'].iloc[0]
+
+    return sleep_mins['exp_time'].min() - first_awake_min
+
 
 def total_activity_sleep(df):
     """
@@ -53,6 +173,39 @@ def total_activity_sleep(df):
     gb = df.groupby(['fish', 'genotype', 'day', 'light'])
     return gb['activity', 'sleep'].sum().reset_index()
 
+
+
+def _column_tup_to_str(ind):
+    """
+    Convert tuple of MultiIndex to string.
+
+    Parameters
+    ----------
+    ind : tuple
+        ind[0]: either 'sleep' or 'activity'
+        ind[1]: int that is the day number
+        ind[2]: bool, True being light, False being dark
+
+    Returns
+    -------
+    output : str
+        Conversion to a single string represnting info in tuple.
+        E.g., ('activity', 6, True) gets converted to
+        'total seconds of activity in day 6'.
+    """
+    if ind[0] == 'activity':
+        string = 'total seconds of activity in '
+    elif ind[0] == 'sleep':
+        string = 'total minutes of sleep in '
+    else:
+        raise RuntimeError('%s is invalid MultiIndex' % ind[0])
+
+    if ind[2]:
+        return string + 'day ' + str(ind[1])
+    else:
+        return string + 'night ' + str(ind[1])
+
+
 def write_total_activity_sleep(df, outfile):
     """
     Write a CSV file with summary of
@@ -76,16 +229,6 @@ def write_total_activity_sleep(df, outfile):
     sum_df.sort_index(axis=0, level=['genotype', 'fish'], inplace=True)
 
     # Rename the column headings from the MultiIndex
-    def tup_to_str(ind):
-        if ind[0] == 'activity':
-            string = 'total seconds of activity in '
-        elif ind[0] == 'sleep':
-            string = 'total minutes of sleep in '
-        if ind[2]:
-            return string + 'day ' + str(ind[1])
-        else:
-            return string + 'night ' + str(ind[1])
-
-    sum_df.columns = [tup_to_str(ind) for ind in sum_df.columns]
+    sum_df.columns = [_column_tup_to_str(ind) for ind in sum_df.columns]
 
     return sum_df
---
+++ tests/test_summarize.py
@@ -0,0 +1,106 @@
+import pytest
+
+import numpy as np
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
+import fishact
+
+
+def test_sleep_latency():
+    df = pd.DataFrame({'exp_time': np.linspace(0.0, 19.0, 20),
+                       'sleep': np.ones(20, dtype=int)})
+
+    assert np.isnan(fishact.summarize._sleep_latency(df))
+
+    df.loc[6, 'sleep'] = 0
+    assert np.isclose(fishact.summarize._sleep_latency(df), 1.0)
+
+    df.loc[7, 'sleep'] = 0
+    assert np.isclose(fishact.summarize._sleep_latency(df), 2.0)
+
+    df.loc[5, 'sleep'] = 0
+    assert np.isclose(fishact.summarize._sleep_latency(df), 3.0)
+
+    df.loc[15, 'sleep'] = 0
+    assert np.isclose(fishact.summarize._sleep_latency(df), 3.0)
+
+    df.loc[0:15, 'sleep'] = 0
+    assert np.isclose(fishact.summarize._sleep_latency(df), 16.0)
+
+    df['sleep'] = np.zeros(len(df))
+    assert np.isnan(fishact.summarize._sleep_latency(df))
+
+
+def test_compute_bouts():
+    df = pd.DataFrame({'exp_time': np.linspace(0.0, 19.0, 20),
+                       'sleep': np.ones(20, dtype=int),
+                       'time': pd.to_datetime(['2017-03-30 14:00:00',
+                                               '2017-03-30 14:01:00',
+                                               '2017-03-30 14:02:00',
+                                               '2017-03-30 14:03:00',
+                                               '2017-03-30 14:04:00',
+                                               '2017-03-30 14:05:00',
+                                               '2017-03-30 14:06:00',
+                                               '2017-03-30 14:07:00',
+                                               '2017-03-30 14:08:00',
+                                               '2017-03-30 14:09:00',
+                                               '2017-03-30 14:10:00',
+                                               '2017-03-30 14:11:00',
+                                               '2017-03-30 14:12:00',
+                                               '2017-03-30 14:13:00',
+                                               '2017-03-30 14:14:00',
+                                               '2017-03-30 14:15:00',
+                                               '2017-03-30 14:16:00',
+                                               '2017-03-30 14:17:00',
+                                               '2017-03-30 14:18:00',
+                                               '2017-03-30 14:19:00']),
+                       'light': [True]*20,
+                       'day': [5]*20})
+
+    correct_df = pd.DataFrame(
+        columns=['day_start', 'day_end', 'light_start', 'light_end',
+                 'bout_start_exp', 'bout_end_exp',
+                 'bout_start_clock', 'bout_end_clock', 'bout_length'])
+    assert_frame_equal(fishact.summarize._compute_bouts(df), correct_df)
+
+    df['sleep'] = np.array([0]*5 + [1]*15,dtype=int)
+    assert_frame_equal(fishact.summarize._compute_bouts(df), correct_df)
+
+    df['sleep'] = np.array([1]*5 + [0]*15,dtype=int)
+    assert_frame_equal(fishact.summarize._compute_bouts(df), correct_df)
+
+    df['sleep'] = np.array([0]*3 + [1]*4 + [0]*13,dtype=int)
+    correct_df = correct_df.append(
+            {'day_start': 5,
+               'day_end': 5,
+               'light_start': True,
+               'light_end': True,
+               'bout_start_exp': 3.0,
+               'bout_end_exp': 7.0,
+               'bout_start_clock': pd.to_datetime('2017-03-30 14:03:00'),
+               'bout_end_clock': pd.to_datetime('2017-03-30 14:07:00'),
+               'bout_length': 4.0}, ignore_index=True)
+    assert_frame_equal(fishact.summarize._compute_bouts(df), correct_df,
+                       check_dtype=False)
+
+    df['sleep'] = np.array([0]*3 + [1]*4 + [0] + [1]*12, dtype=int)
+    assert_frame_equal(fishact.summarize._compute_bouts(df), correct_df,
+                       check_dtype=False)
+
+    df['sleep'] = np.array([0]*3 + [1]*4 + [0]*2 + [1]*10 + [0],dtype=int)
+    correct_df = pd.DataFrame(
+            {'day_start': [5, 5],
+               'day_end': [5, 5],
+               'light_start': [True, True],
+               'light_end': [True, True],
+               'bout_start_exp': [3.0, 9.0],
+               'bout_end_exp': [7.0, 19.0],
+               'bout_start_clock': pd.to_datetime(['2017-03-30 14:03:00',
+                                                   '2017-03-30 14:09:00']),
+               'bout_end_clock': pd.to_datetime(['2017-03-30 14:07:00',
+                                                 '2017-03-30 14:19:00']),
+               'bout_length': [4.0, 10.0]})
+    correct_df = correct_df.sort_index(axis=1)
+    assert_frame_equal(fishact.summarize._compute_bouts(df).sort_index(axis=1),
+                       correct_df, check_dtype=False)
